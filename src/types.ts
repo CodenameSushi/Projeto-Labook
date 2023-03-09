348bb3f4b2f3614097aae239963fb358b320394a
CodenameSushi <fhenrique08@gmail.com>
2023-03-09 20:20:01 -0300

    Models, Types, DTOs

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,46 @@ export interface TokenPayload {
 	name: string,
     role: USER_ROLES
 }
+
+
+export interface PostModel {
+    id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    createdAt: string,
+    updatedAt: string,
+    creator: {
+        id: string,
+        name: string
+    }
+}
+
+export interface PostDB {
+    id: string,
+    creator_id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    created_at: string,
+    updated_at:string
+}
+
+export interface UserDB {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: USER_ROLES,
+    created_at: string
+}
+
+export interface UserModel {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: USER_ROLES,
+    createdAt: string
+}
+
